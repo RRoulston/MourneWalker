916d3e6e2ebe4f55882073cb5b32dad6e1db5c92
Ralph Roulston <ralphroulston@live.co.uk>
2018-11-24 15:31:36 +0000

    Mourne Walker App Set Up Live weather updates added

---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather'
+/**
+ * Generated class for the WeatherPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-weather',
+  templateUrl: 'weather.html',
+})
+export class WeatherPage {
+  temperatureData: any;
+  weatherDescription: any;
+  weatherIcon: any;
+  weatherHumidity: any;
+  windSpeed: any;
+  windDirection: any;
+  sunrise: any;
+  sunset: any;
+  lastUpdated: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public weatherProvider: WeatherProvider) {
+    this.getWeather();
+  }
+
+  ionViewDidLoad() {
+  console.log('ionViewDidLoad WeatherPage');
+  }
+
+  getWeather(){
+    this.weatherProvider.getWeatherData().subscribe(weatherData => {
+      console.log(weatherData)
+      this.temperatureData = weatherData.main.temp;
+      this.weatherDescription = weatherData.weather[0].description;
+      this.weatherIcon = weatherData.weather[0].icon;
+      this.weatherHumidity = weatherData.main.humidity;
+      this.windSpeed = weatherData.wind.speed;
+      this.windDirection = weatherData.wind.deg;
+      this.sunrise = weatherData.sys.sunrise;
+      this.sunset = weatherData.sys.sunset;
+      this.lastUpdated = weatherData.dt;
+    });
+  }
+
+}
